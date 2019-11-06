87afe6766ac6f82fd64bc36cfb4d440a79ac727e
isaacwashere <ijcurtiss7@gmail.com>
2019-11-06 15:10:59 -0500

    pass Result the total number of questions so the score is accurate

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Result = ({score, playAgain}) => (
+const Result = ({score, playAgain, numQuestions}) => (
   <div className="score-board">
-    <div className="score">You scored {score} / 5 correct answers!</div>
+    <div className="score">You scored {score}/{numQuestions} correct answers!</div>
     <button className="playBtn" onClick={playAgain}>
       Play again!
     </button>
